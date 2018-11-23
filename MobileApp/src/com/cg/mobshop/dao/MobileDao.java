fb86b8fe2e8742f19e703cc3c8fad9dfbbc8a6e6
dibyashankarsatpathy <43958143+dibyashankarsatpathy@users.noreply.github.com>
2018-11-23 15:09:11 +0530

    Add files via upload

---
+++ MobileApp/src/com/cg/mobshop/dao/MobileDao.java
@@ -0,0 +1,14 @@
+package com.cg.mobshop.dao;
+
+import java.util.List;
+
+import com.cg.mobshop.dto.Mobiles;
+
+public interface MobileDao {
+	
+	public List<Mobiles> getAllMobiles();
+	public Mobiles getMobileDetails(int mobid);
+	public int updateMobiles(Mobiles mob);
+	public List<Mobiles> delete(int mobid);
+
+}
